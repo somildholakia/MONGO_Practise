a233e05636a1070aacc182267a7c79e737975b8f
somildholakia <somildholakia0852@gmail.com>
2026-01-18 18:39:23 +0530

    update

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test")
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
