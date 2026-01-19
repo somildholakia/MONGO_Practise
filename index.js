38a39ed6f19e22d3fa4ffd85ddf08211c5a0d820
somildholakia <somildholakia0852@gmail.com>
2026-01-19 21:04:22 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
 
 main()
     .then(res => {
@@ -14,6 +15,22 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+
+
+let chat1 = new Chat({
+    from: "somil",
+    to: "krishna",
+    msg: "Send the notes bro",
+    created_at: new Date(),
+});
+
+chat1.save().then(res => {
+    console.log(res);
+});
+
+
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
--- models/chat.js
+++ models/chat.js
@@ -20,3 +20,6 @@ const chatSchema = new mongoose.Schema({
     }
 });
 
+const Chat = mongoose.model("Chat",chatSchema);
+
+module.exports = Chat;
