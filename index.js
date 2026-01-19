5febb79ad06c7e20309d146c2ad3201e8361f234
somildholakia <somildholakia0852@gmail.com>
2026-01-19 20:36:53 +0530

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ main()
     })
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
 app.use(express.urlencoded({extended: true}));
