ec52c27814a183766c02103df14e4abffe99f0c8
somildholakia <somildholakia0852@gmail.com>
2026-01-21 18:06:33 +0530

    update

--- index.js
+++ index.js
@@ -61,3 +61,17 @@ app.post("/chats",(req,res) => {
     })
     res.redirect("/chats");
 })
+
+app.get("/chats/:id/edit", async (req,res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+})
+
+app.put("/chats/:id", async (req,res) => {
+    let {id} = req.params;
+    let {newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id,{msg: newMsg}, {runValidators: true, new: true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+})
